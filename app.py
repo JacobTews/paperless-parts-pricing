230b1afb3d72a005cc7b763a6ced9d77dc1544e7
JacobTews <j.p.tews@gmail.com>
2025-08-04 13:46:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-import logging
-logging.basicConfig(level=logging.DEBUG)
-print("App is starting...")
+# import logging
+# logging.basicConfig(level=logging.DEBUG)
+# print("App is starting...")
 
-import os
-from flask import Flask
-app = Flask(__name__)
+# import os
+# from flask import Flask
+# app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello from a containerized Python app!"
+# @app.route('/')
+# def home():
+#     return "Hello from a containerized Python app!"
 
 # if __name__ == "__main__":
 #     port = int(os.environ.get("PORT", 5000))  # Use Railway-provided port or fallback to 5000
 #     app.run(host="0.0.0.0", port=port)
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Railway!"
