d84573c55c4fb7db78c225abdf142347d76ba8bb
JacobTews <j.p.tews@gmail.com>
2025-08-04 13:28:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,4 +7,5 @@ def home():
     return "Hello from a containerized Python app!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    port = int(os.environ.get("PORT", 5000))  # Use Railway-provided port or fallback to 5000
+    app.run(host="0.0.0.0", port=port)
